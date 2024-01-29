8eae5f6870a92ee0e17b1b1594168eab186320f1
kapilasparrot <136551988+kapilasparrot@users.noreply.github.com>
2024-01-29 09:57:48 +0530

    Update basic within-pop model.R

--- src/basic within-pop model.R
+++ src/basic within-pop model.R
@@ -1,9 +1,9 @@
 #Code for within-population model
 step_within_population <- function(n_t, phi_J, alpha_J, phi_P, alpha_P, mu, f, phi_A) {
   #Transition matrix for within poopulation
-  W <- matrix(c(phi_J * (1 - alpha_J), phi_J * alpha_J, 0,
-                phi_P * (1 - alpha_P) * (1 - mu), phi_P * alpha_P * (1 - mu), f,
-                0, 0, phi_A), nrow = 3, byrow = TRUE)
+  W <- matrix(c(phi_J * (1 - alpha_J), 0, f,
+                phi_J * alpha_J, phi_P * (1-alpha_P) * (1 - mu), 0,
+                0, phi_P * alpha_P * (1 - mu), phi_A), nrow = 3, byrow = TRUE)
   
   #1 time step for within-population
   n_t_plus_1 <- W %*% n_t
