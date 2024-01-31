f3b2d1fda4710ab3739c6c8f7f438497f51c2373
Ben Phillips <ben.l.phillips@curtin.edu.au>
2024-01-31 16:54:23 +0800

    make survival_prob a switch remove redundant line

--- src/modelFunctions.R
+++ src/modelFunctions.R
@@ -24,17 +24,15 @@
 # Modified recursion for the within-population model with cumulative offspring affecting survival
 step_within_population <- function(n_t, phi_J, alpha_J, phi_P, alpha_P, mu, f, phi_A, cumulative_offspring, survival_threshold) {
   # Calculate the survival probability based on cumulative offspring
-  survival_prob <- ifelse(cumulative_offspring < survival_threshold, phi_J, 0)
+  survival_prob <- ifelse(cumulative_offspring < survival_threshold, 1, 0)
   
   # Transition matrix for within population with density-dependent survival
-  W <- matrix(c(survival_prob * (1 - alpha_J), 0, f,
-                survival_prob * alpha_J, phi_P * (1 - alpha_P) * (1 - mu), 0,
+  W <- matrix(c(survival_prob * phi_J * (1 - alpha_J), 0, f,
+                survival_prob * phi_J * alpha_J, phi_P * (1 - alpha_P) * (1 - mu), 0,
                 0, phi_P * alpha_P * (1 - mu), phi_A), nrow = 3, byrow = TRUE)
   
   # Calculate the number of offspring produced in this time step
   offspring_produced <- f * n_t[3]  # Adults contribute to offspring based on fA
-  offspring_produced <- offspring_produced_juveniles + offspring_produced_adults
-  
   
   # Update cumulative offspring
   cumulative_offspring <- cumulative_offspring + offspring_produced
