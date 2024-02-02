53aa1a21806fbe2728a7e89189b994f5d31ff4f2
kapilasparrot <136551988+kapilasparrot@users.noreply.github.com>
2024-02-02 10:58:24 +0530

    Update basic within-pop model.R
    parameter updates

--- src/basic within-pop model.R
+++ src/basic within-pop model.R
@@ -22,14 +22,45 @@
 source("src/modelFunctions.R")
 
 
-# Sample parameters
-phi_J <- 0.9
-alpha_J <- 0.1
-phi_P <- 0.8
-alpha_P <- 0.2
-mu <- 0.05
-f <- 2.0
-phi_A <- 0.95
+# Parameters
+alpha_J_temp <- function(temperature, lower = 15, upper = 31){
+  rate <- -0.0273 + 0.0023*temperature
+  prob <- 1-exp(-rate)
+  ifelse(temperature>lower & temperature < upper, prob, 0)
+}
+alpha_P_temp <- function(temperature, lower = 15, upper = 31){
+  DD <- 136 #degree days from walgama
+  beta <- 1/DD # implied slope of rate on temperature
+  alpha <- -15*beta # implied intercept
+  rate <- alpha + beta*temperature
+  prob <- 1-exp(-rate)
+  ifelse(temperature>lower & temperature < upper, prob, 0)
+}
+alpha_J <- alpha_J_temp(temps)
+plot(alpha_J~temps, 
+     type = "l", 
+     xlab = "Temperature", 
+     ylab = quote(italic(alpha[J])),
+     bty = "l")
+phi_J <- function(temperature){
+    TPC.q(temps, rmax = 0.99, Trmax = 29.5, acc = 80, dec.prop = 0.15)
+  }
+alpha_P <-  alpha_P_temp(temps)
+  plot(alpha_P~temps, 
+       type = "l", 
+       xlab = "Temperature", 
+       ylab = quote(italic(alpha[P])),
+       bty = "l")
+    ifelse(temperature>lower & temperature < upper, prob, 0)
+  }
+
+phi_P <- 0.97
+phi_J <- function(temperature){
+    TPC.q(temps, rmax = 0.99, Trmax = 29.5, acc = 80, dec.prop = 0.15)
+  }
+phi_A <- 0.97
+mu <- 0
+f <- 0.69
 
 # Initial population 
 n_initial <- c(100, 50, 20)  # Sample initial population size
