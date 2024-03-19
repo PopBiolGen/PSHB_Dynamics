19cedb947c82a6b8e6fb3afa2c3dde7ec6bcb6c2
Nick Golding <goldingn35@gmail.com>
2024-03-19 09:58:02 +0800

    speed up sparse dispersal a little

--- src/stochastic_landscape.R
+++ src/stochastic_landscape.R
@@ -6,6 +6,17 @@ library(sf)
 library(terra)
 library(geodata)
 
+# given random variables z (with standard normal distribution a priori), and
+# Poisson rate parameter lambda, return a strictly positive continuous random
+# variable with the same mean and variance as a poisson random variable with
+# rate lambda, by approximating the poisson as a lognormal distribution.
+lognormal_continuous_poisson <- function(lambda, z) {
+  sigma <- sqrt(log1p(lambda / exp(2 * log(lambda))))
+  # sigma2 <- log1p(1 / lambda)
+  mu <- log(lambda) - sigma^2 / 2
+  exp(mu + z * sigma)
+}
+
 # path to the data
 data_dir <- "../mozambique_example/data/"
 
@@ -75,13 +86,136 @@ min(cluster_pops$population_density)
 # check totals match
 expected_pop <- global(moz_pop, sum, na.rm = TRUE)[1, 1]
 observed_pop <- sum(cluster_pops$population_density)
-stopifnot( abs(observed_pop - expected_pop) < 1e-6)
+stopifnot(abs(observed_pop - expected_pop) < 1e-6)
 
 # define an adjacency matrix for dispersal
 adjacency <- adjacent(voronoi)
 head(adjacency)
 
+# set up dispersal information
+n_routes <-  table(adjacency[, "from"])
+dispersal_info <- list(
+  # total fraction dispersing from each cluster on each timestep
+  fraction_dispersing = 0.1,
+  # the routes between clusters
+  routes = adjacency,
+  # the fraction *of those leaving a cluster* that go via each route
+  dispersal_fraction_normalised = (1 / n_routes)[adjacency[, "from"]]
+)
 
+# explicit, but slightly inefficient implementation of sparse dispersal
+dispersal_sparse_slow <- function(cluster_state, dispersal_info) {
+  
+  # expected number leaving each cluster
+  leaving_cluster_expected <- cluster_state *
+    dispersal_info$fraction_dispersing
+  
+  # expected number going via each route
+  leaving_route_expected <- leaving_cluster_expected[dispersal_info$routes[, "from"]] *
+    dispersal_info$dispersal_fraction_normalised
+  
+  # sum over all routes by cluster of arrival to get the expected numebr arriving
+  # at each
+  
+  # number travelling via each adjacency
+  arriving_cluster_expected <- tapply(leaving_route_expected,
+                                      dispersal_info$routes[, "to"],
+                                      FUN = "sum")
+  
+  # sum up dispersals (note this could be made faster by having self-dispersal in
+  # the lookup)
+  cluster_state_expected -
+    leaving_cluster_expected +
+    arriving_cluster_expected
+  
+}
+
+# explicit, but slightly inefficient implementation of sparse dispersal
+dispersal_sparse <- function(cluster_state, dispersal_info) {
+  
+  # combine dispersal info to get a single lookup of dispersal and non-dispersal
+  dispersal_info_new <- dispersal_info
+
+  n_clusters <- length(cluster_state)
+  routes_self <- cbind(from = seq_len(n_clusters),
+                       to = seq_len(n_clusters))
+  
+  other_dispersal_vec <- dispersal_info$dispersal_fraction_normalised *
+    dispersal_info$fraction_dispersing
+  
+  self_dispersal <- 1 - dispersal_info$fraction_dispersing
+  if (length(self_dispersal) == n_clusters) {
+    self_dispersal_vec <- self_dispersal
+  } else {
+    self_dispersal_vec <- rep(self_dispersal, n_clusters)
+  }
+  
+  # combine into a single sparse matrix
+  routes_all <- rbind(dispersal_info$routes, routes_self)
+  dispersal_vec <- c(other_dispersal_vec, self_dispersal_vec)
+  
+  # expected number going via each route
+  route_state_expected <- cluster_state[routes_all[, "from"]] * dispersal_vec
+  
+  # sum over all routes by cluster of arrival to get the expected number arriving
+  # at each
+  tapply(route_state_expected,
+         routes_all[, "to"],
+         FUN = "sum")
+  
+}
+
+# remind myself of sparse dispersal thingo
+
+# proportion dispersing from each cluster at each timestep
+
+# fraction of those dispersing from each cluster that use each route
+
+# get all pixels
+
+# pixel and cluster ids
+cell_id <- cells(moz_mask)
+cluster_id <- terra::extract(clusters, cell_id)$cluster
+n_cells <- length(cell_id)
+
+# fake state
+state <- runif(n_cells)
+
+# fake growth rates
+r <- abs(rnorm(n_cells, 1, 0.25))
+
+# fake innovations (cluster level)
+z <- rnorm(n_clusters)
+
+# apply growth rates to get pixel-level expected cells
+state_grown_expected <- state * r
+
+# aggregate to get expected total state in each clusters
+cluster_state_expected <- tapply(state_grown_expected,
+                                 cluster_id,
+                                 FUN = "sum")
+
+# do sparse expected dispersal to new states
+cluster_new_state_expected <- dispersal_sparse(
+  cluster_state = cluster_state_expected,
+  dispersal_info = dispersal_info)
+
+cluster_new_state_expected2 <- dispersal_sparse_slow(
+  cluster_state = cluster_state_expected,
+  dispersal_info = dispersal_info)
+
+# check these are the same
+max(abs(cluster_new_state_expected - cluster_new_state_expected2)) < 1e-6
+
+# apply stochasticity to the expected new state
+cluster_new_state <- lognormal_continuous_poisson(cluster_new_state_expected, z)
+
+# compute ratios in each cluster, to apply the same dispersal effect and
+# stochastic demographic change across all pixels
+cluster_ratios <- cluster_new_state / cluster_old_state
+
+# compute the new pixel-level states
+new_state <- state * cluster_ratios[cluster_id]
 
 
 # build greta model
