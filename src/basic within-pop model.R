954002975c3f8733249f30fd6c903672944b9df3
Ben Phillips <ben.l.phillips@curtin.edu.au>
2024-05-16 14:35:39 +0800

    examine different locations, output growth rate

--- src/basic within-pop model.R
+++ src/basic within-pop model.R
@@ -18,10 +18,10 @@ source("src/TPCFunctions.R")
 source("src/modelFunctions.R")
 
 # Get tree temperature data
-locLat <- -32.005892
-locLong <- 115.896019
+locLat <- -17.957387
+locLong <- 122.215386
 
 
 yearSim <- run_year(lat = locLat, long = locLong, make_plot = TRUE)
 
-
+yearSim$growthRate
