32e01ec1292f56f24a4e8f3bbf327ca2510d8738
Andrew Coates <a.coates2@student.unimelb.edu.au>
2024-07-08 09:12:11 +0930

    Add required functions and packages (from 'basic within-pop model'. Lat & Long for Perth area

--- src/growth_rate_map.R
+++ src/growth_rate_map.R
@@ -6,13 +6,20 @@ library(ggplot2)
 library(ozmaps)
 library(sf)
 library(viridis)
+# From 'basic within-pop models.R'
+library(dplyr)
+library(lubridate)
+## Load up the functions
+source("src/TPCFunctions.R")
+source("src/modelFunctions.R")
+
 # WA map
 sf_oz <- subset(ozmap("states"), NAME=="Western Australia")
 
 # For now, manually select coordinate ranges to construct a retangular grid covering whole area
 # SILO data resolution = 0.05 x 0.05 degrees
-lat <- c(seq(-32.9, -31.2, by=0.05)) # Latitude range
-lon <- c(seq(115.35, 116.4, by=0.05)) # Longitude range
+lat <- c(seq(-33.3, -31, by=0.05)) # Latitude range
+lon <- c(seq(115.2, 116.8, by=0.05)) # Longitude range
 grid <- (expand.grid(lon, lat)) # Grid containing each lat & lon combination
 colnames(grid) <- c("lon", "lat")
 # Check on map
@@ -41,9 +48,8 @@ ggplot(data = sf_oz) +
   scale_x_continuous(limits=c(min(lon),max(lon)))+
   scale_y_continuous(limits=c(min(lat),max(lat)))
 
-grid2 <- as.matrix(grid[,-c(3,4)]) # Subset just lat & lon, convert to matrix
-
 # Create output matrix
+grid2 <- as.matrix(grid[,-c(3,4)]) # Subset just lat & lon, convert to matrix
 outputs_grid <- matrix(0, nrow=nrow(grid), ncol=5)
 outputs_grid[c(1:nrow(outputs_grid)),c(1:2)] <- grid2 # Insert lat & lon for each coord
 colnames(outputs_grid)<- c("lon","lat","J","P","A") # Leave remaining columns to insert J, P & A growth rates
