a3af7f8e3bfaada69373a821f43a56d52bb778de
Andrew Coates <a.coates2@student.unimelb.edu.au>
2024-07-10 10:31:47 +0930

    Foreach function now working (PC space issue?). Add working script across to this main one.

--- src/growth_rate_map.R
+++ src/growth_rate_map.R
@@ -14,14 +14,16 @@ source("src/TPCFunctions.R")
 source("src/modelFunctions.R")
 # Run parallel over multiple cores
 library(foreach)
+library(parallel)
+library(doParallel)
 
 # WA map
 sf_oz <- subset(ozmap("states"), NAME=="Western Australia")
 
 # For now, manually select coordinate ranges to construct a retangular grid covering whole area
 # SILO data resolution = 0.05 x 0.05 degrees
-lat <- c(seq(-33.3, -31, by=0.05)) # Latitude range
-lon <- c(seq(115.2, 116.8, by=0.05)) # Longitude range
+lat <- c(seq(-33.3, -33.1, by=0.05)) # Latitude range
+lon <- c(seq(116.2, 116.4, by=0.05)) # Longitude range
 grid <- (expand.grid(lon, lat)) # Grid containing each lat & lon combination
 colnames(grid) <- c("lon", "lat")
 # Check on map
@@ -52,19 +54,40 @@ ggplot(data = sf_oz) +
 
 # Create output matrix
 grid2 <- as.matrix(grid[,-c(3,4)]) # Subset just lat & lon, convert to matrix
-outputs_grid <- matrix(0, nrow=nrow(grid), ncol=5)
+outputs_grid <- matrix(0, nrow=nrow(grid), ncol=3)
 outputs_grid[c(1:nrow(outputs_grid)),c(1:2)] <- grid2 # Insert lat & lon for each coord
-colnames(outputs_grid)<- c("lon","lat","J","P","A") # Leave remaining columns to insert J, P & A growth rates
+colnames(outputs_grid)<- c("lon","lat","A") # Leave remaining column empty to A growth rates
 
-#### FOR LOOP Version ####
-for(i in 1:nrow(outputs_grid)){
-  locLong <- outputs_grid[i, "lon"]
-  locLat <- outputs_grid[i, "lat"]
-  yearSim <- run_year(lat = locLat, long = locLong, make_plot = FALSE) # FROM 'basic within-pop model.R'
-  rate_grid <- yearSim$growthRate # Calc mean growth rates
-  outputs_grid[i,c(3:5)] <- rate_grid # Insert growth rates into output matrix (with corresponding coords)
-}
-# 
+### FOREACH method (run parallel over multiple cores)
+n.cores <- 5 # Assign number cores (my PC has 8)
+my.cluster <- parallel::makeCluster(
+  n.cores, 
+  type = "PSOCK"
+)
+print(my.cluster) #check cluster definition
+doParallel::registerDoParallel(cl = my.cluster) #register it to be used by %dopar%
+
+#### Create vector of ADULT growth rates
+# Similar to for loop, but runs over multiple cores then combines outputs
+out_v <- foreach(i = 1:nrow(outputs_grid), 
+                 .combine='c', # Combine outputs into vector (can also use 'cbind' or 'rbind' to create matrix)
+                 .packages = c("httpcode", # Need to install packages on all Worker cores
+                               "urltools",
+                               "ggplot2",
+                               "ozmaps",
+                               "sf",
+                               "viridis",
+                               "dplyr",
+                               "lubridate",
+                               "foreach")) %dopar% {
+                                 locLong <- outputs_grid[i,"lon"]
+                                 locLat <- outputs_grid[i,"lat"]
+                                 yearSim <- run_year(lat = locLat, long = locLong, make_plot = FALSE) # FROM 'basic within-pop model.R'
+                                 rate_grid <- yearSim$growthRate # Calc mean growth rates
+                                 return(rate_grid[3]) # For now, just use ADULT GROWTH RATE values (similar rates across all stages)
+                               }
+
+outputs_grid[,"A"] <- out_v # Add growth rate vector into output matrix
 
 # Plot output
 ggplot(data = sf_oz) + 
@@ -80,3 +103,17 @@ write.csv(outputs_grid, # Save output
           file = "out/perth_grid.csv", col.names = T )
 
 
+
+#### FOR LOOP Version ####
+outputs_grid <- matrix(0, nrow=nrow(grid), ncol=5)
+outputs_grid[c(1:nrow(outputs_grid)),c(1:2)] <- grid2 # Insert lat & lon for each coord
+colnames(outputs_grid)<- c("lon","lat","J","P","A") # Leave remaining columns to insert J, P & A growth rates
+
+for(i in 1:nrow(outputs_grid)){
+  locLong <- outputs_grid[i, "lon"]
+  locLat <- outputs_grid[i, "lat"]
+  yearSim <- run_year(lat = locLat, long = locLong, make_plot = FALSE) # FROM 'basic within-pop model.R'
+  rate_grid <- yearSim$growthRate # Calc mean growth rates
+  outputs_grid[i,c(3:5)] <- rate_grid # Insert growth rates into output matrix (with corresponding coords)
+}
+# 
