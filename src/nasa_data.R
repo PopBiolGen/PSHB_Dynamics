52f18bf99c8b15b995b42a03ece739aa2aa8442f
coates-a <andrewlindsaycoates@gmail.com>
2024-10-16 10:46:15 +0800

    Set coords for Sth African cities. Run sim for locations; plot daily growht

--- src/nasa_data.R
+++ src/nasa_data.R
@@ -16,23 +16,107 @@ tree_temp_model_pars <- coef(mod_fit)
 mu_est <- 0
 
 # South Africa coords
+# Locations from van Rooyen et al. map (https://www.fabinet.up.ac.za/pdf/PSHB/7-PSHB%20distribution%20map%202021-03-04.pdf)
+# Matched against records from iNaturalist
 
 George <- c(long = 22.46,
             lat = -33.95)
 
-run_loc <- function(loc) {
-  locLong <- loc["long"]
-  locLat <- loc["lat"]
-  run_year(lat = locLat, long = locLong, make_plot = TRUE)
-  }
+Durban_Kloof <- c(long = 30.83,
+                  lat = -29.76)
+
+Jo <- c(long = 28.06,
+            lat = -26.16)
+
+# Need to fix - when running this function, locLong & locLat aren't saved...
+# run_loc <- function(loc) {
+#  locLong <- loc["long"]
+#  locLat <- loc["lat"]
+#  run_year(lat = locLat, long = locLong, make_plot = TRUE)
+#  }
 
 
-# Run numerical sims
-George_sim <- run_loc(George)
+#George
+locLong <- George["long"]
+locLat <- George["lat"]
+George_sim <- run_year(lat = locLat, long = locLong, make_plot = TRUE)
 George_sim$growthRate
 
+# Durban
+locLong <- Durban_Kloof["long"]
+locLat <- Durban_Kloof["lat"]
+Durban_Kloof_sim <- run_year(lat = locLat, long = locLong, make_plot = TRUE)
+Durban_Kloof_sim$growthRate
+
+# Johannesburg
+locLong <- Jo["long"]
+locLat <- Jo["lat"]
+Jo_sim <- run_year(lat = locLat, long = locLong, make_plot = TRUE)
+Jo_sim$growthRate
 
 
+##### Grid plot of cities - daily growth rate ####
+
+city_coords <- data.frame(city = c('George', 'Durban', 'Johannesburg'),
+                          lat = c(George["lat"], Durban_Kloof["lat"], Jo["lat"]),
+                          lon = c(George["long"], Durban_Kloof["long"], Jo["long"]))
+
+# Max & min growth same as Aus cities:
+mingrow <- -0.02
+maxgrow <- 0.1
+
+# Pick a location
+dev.off()
+par(mfrow = c(1, 4))
+
+for(i in 1:nrow(city_coords)){
+  
+  locLat <- city_coords$lat[i]
+  locLong <- city_coords$lon[i]
+  city_name <- city_coords$city[i]
+  
+  yearSim_city <- run_year(lat = locLat, long = locLong, 
+                           make_plot = FALSE)
+  
+  population_data <- yearSim_city$popDat
+  
+  # By stage
+  juv_vec <- population_data[1,]
+  pre_adult_vec <- population_data[2,]
+  ad_vec <- population_data[3,]
+  # Calculate growth rates for all life stages
+  juv_growth_rate <- diff(log(juv_vec))
+  pre_adult_growth_rate <- diff(log(pre_adult_vec))
+  ad_growth_rate <- diff(log(ad_vec))
+  # Smooth the growth rate curves using a moving average
+  window_size <- 5
+  juv_growth_rate_smoothed <- stats::filter(juv_growth_rate, rep(1/window_size, window_size), sides = 2)
+  pre_adult_growth_rate_smoothed <- stats::filter(pre_adult_growth_rate, rep(1/window_size, window_size), sides = 2)
+  ad_growth_rate_smoothed <- stats::filter(ad_growth_rate, rep(1/window_size, window_size), sides = 2)
+  
+  plot(juv_growth_rate_smoothed, type = "l", 
+       main = city_name, 
+       xlab = "Day of year", 
+       ylab = "Growth rate", 
+       col = "grey75", 
+       lwd = 1.8, 
+       ylim = c(mingrow, maxgrow),
+       bty = "l")
+  lines(pre_adult_growth_rate_smoothed, col = "grey50", lwd = 1.8)
+  lines(ad_growth_rate_smoothed, col = "grey25", lwd = 1.8)
+  #  legend("bottomright", legend = c("Juveniles", "Pre-adults", "Adults"), 
+  #        col = c("grey75", "grey50", "grey25"), 
+  #       lty = 1, lwd = 2, cex = 0.75)
+  abline(h=0, lty=2)
+  axis(side = 1, at = c(seq(0, 300, by=100))) 
+  axis(side = 2, at = c(15, 50, 75, 100))
+}
+
+# Plot just legend:
+plot(NULL ,xaxt='n',yaxt='n',bty='n',ylab='',xlab='', xlim=0:1, ylim=0:1)
+legend("center", legend = c("Juveniles", "Pre-adults", "Adults"), 
+       col = c("grey75", "grey50", "grey25"), 
+       lty = 1, lwd = 2, cex = 1.6, bty='n')
 
 
 ##############################################################
