f57f7704e794eca8139e09718fe5b6e6033536d3
coates-a <andrewlindsaycoates@gmail.com>
2024-11-18 09:52:36 +0800

    Add 'model' and mu terms

--- src/basic within-pop model.R
+++ src/basic within-pop model.R
@@ -13,14 +13,20 @@
 library(dplyr)
 library(lubridate)
 library(maps)
+library(ozmaps)
+library(sf)
+sf_oz <- subset(ozmap("country"))
 
 ## Load up the functions
 source("src/TPCFunctions.R")
 source("src/modelFunctions.R")
 
+model <- "weighted_mean" # if model == 'weighted_mean' use weighted mean model (with greta coeff) to predicts tree temp
+# otherwise use 'mod_fit' lm
+
 # Pick a location
-locLat <- -32.005892
-locLong <- 115.896019
+locLat <- -31.9616
+locLong <- 115.8317
 
 # Load data for temperature function (from temperature-prediction-function.R -- spartan can't load Rdata file)
 merge_temp <- read.csv('src/temperatures/merge_temp.csv')
@@ -28,7 +34,11 @@ mod_fit <- lm(mean_d ~ air_tmax*rh_tmax + ma30*rh_tmax, data = merge_temp)
 tree_temp_model_pars <- coef(mod_fit)
 
 # Assign mu parameter
-mu_est <- 0
+mu_disp_est <- 0 # estimated mu parameter (proportion P dispersing)
+phi_mu_est <- 1 # estimated phi_mu (proportion survival during dispersal)
+# mu_est <- 0
+mu_est <- mu_disp_est * (1 - phi_mu_est) # new 'mu' estimate is the proportion of P lost through dispersal mortality (assuming net incoming vs outgoing P = 0)
+
 
 # Run numerical sims
 yearSim <- run_year(lat = locLat, long = locLong, make_plot = TRUE)
