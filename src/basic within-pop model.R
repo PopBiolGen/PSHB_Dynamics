218083f2dfba48352ea10b5b290069534b023261
coates-a <andrewlindsaycoates@gmail.com>
2025-03-12 15:30:31 +0800

    Hash out (#) merge_temp -> only need for previous tree temp linear model

--- src/basic within-pop model.R
+++ src/basic within-pop model.R
@@ -30,14 +30,15 @@ locLong <- 115.8317
 
 country <- "Australia"
 
+# Previous LM approach for tree temp
 # Load data for temperature function (from temperature-prediction-function.R -- spartan can't load Rdata file)
-merge_temp <- read.csv('src/temperatures/merge_temp.csv')
-mod_fit <- lm(mean_d ~ air_tmax*rh_tmax + ma30*rh_tmax, data = merge_temp)
-tree_temp_model_pars <- coef(mod_fit)
+# merge_temp <- read.csv('src/temperatures/merge_temp.csv')
+# mod_fit <- lm(mean_d ~ air_tmax*rh_tmax + ma30*rh_tmax, data = merge_temp)
+# tree_temp_model_pars <- coef(mod_fit)
 
 # Assign mu parameter
-mu_disp_est <- 0.5 # estimated mu parameter (proportion P dispersing)
-phi_mu_est <- 0.8 # estimated phi_mu (proportion survival during dispersal)
+mu_disp_est <- 0 # estimated mu parameter (proportion P dispersing)
+phi_mu_est <- 1 # estimated phi_mu (proportion survival during dispersal)
 # mu_est <- 0
 mu_est <- mu_disp_est * (1 - phi_mu_est) # new 'mu' estimate is the proportion of P lost through dispersal mortality (assuming net incoming vs outgoing P = 0)
 
