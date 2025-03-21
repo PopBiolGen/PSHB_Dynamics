f5a9ba9701c0f9091f3d447acffdf27d1caa7286
coates-a <andrewlindsaycoates@gmail.com>
2025-03-21 12:05:37 +0930

    Plotting TIFF from Li et al

--- src/maps/climatch_raster.R
+++ src/maps/climatch_raster.R
@@ -4,7 +4,85 @@ library(terra)
 library(tidyterra)
 library(ozmaps)
 library(viridis)
+library(ggpubr)
 sf_oz <- subset(ozmap("country"))
+
+# Upload our model results
+mu0 <- read.csv("out/files/mu_0/Aus_mu0.csv")
+mu0.4 <- read.csv("out/files/mu_0.4/Aus_mu0.4.csv")
+
+### Li et al. 2025 (Fuzzy logic, GAM)
+
+library(tidyterra)
+
+rast("src/maps/GAM_Favorability.tiff")
+rr <- rast("src/maps/GAM_Favorability.tiff")
+res(rr) # Resoluation
+crs(rr, proj=TRUE)
+crs(rr) <- "+proj=lcc +lon_0=135 +lat_0=-30 +lat_1=-10 +lat_2=-45 +datum=WGS84" # Ozmaps CRS
+crs(rr)
+plot(rr)
+
+Li.plot <- ggplot() +
+  geom_spatraster(data = rr)+
+  scale_fill_viridis(name = "Climate suitability score\n",
+                     option= "inferno",
+                     limits=c(0, 
+                              1),
+                     breaks=c(seq(0, 1, by=.2)),
+                     labels=c(seq(0, 1, by=.2)),
+                     na.value=NA)+
+  coord_sf(
+    xlim = c(min(mu0$lon),
+             max(mu0$lon)),
+    ylim = c(min(mu0$lat),
+             max(mu0$lat))) +
+  
+  ggtitle("B)")+
+  theme(panel.background = element_blank(),
+        axis.line = element_blank(), 
+        axis.text = element_blank(), 
+        axis.ticks = element_blank(), 
+        axis.title = element_blank(),
+        plot.title = element_text(size=19),
+        legend.text = element_text(size=12),
+        legend.key.size = unit(0.8, 'cm'),
+        legend.title = element_text(size=14))
+Li.plot
+
+### Our model
+
+mech.plot <- ggplot(data = sf_oz) + 
+  geom_tile(data = mu0,
+            aes(x=lon, y=lat, fill=A_growth)) +
+  scale_fill_viridis(name = "Mean daily population\ngrowth rate (adults)\n",
+                     option= "inferno",
+                     limits=c(-0.026, 
+                              0.076),
+                     breaks=c(seq(-0.025, 0.075, by=0.025)),
+                     labels=c(seq(-0.025, 0.075, by=0.025)))+
+  
+  coord_sf(
+    xlim = c(min(mu0$lon),
+             max(mu0$lon)),
+    ylim = c(min(mu0$lat),
+             max(mu0$lat))) +
+  ggtitle("A)")+
+  theme(panel.background = element_blank(),
+        axis.line = element_blank(), 
+        axis.text = element_blank(), 
+        axis.ticks = element_blank(), 
+        axis.title = element_blank(),
+        plot.title = element_text(size=19),
+        legend.text = element_text(size=12),
+        legend.key.size = unit(0.8, 'cm'),
+        legend.title = element_text(size=14))
+mech.plot
+
+
+ggarrange(mech.plot, Li.plot, ncol=2)
+
+### From Climatch
 
 infile <- "dat/target.asc"
 #infile <- "dat/target.txt"
@@ -82,6 +160,30 @@ ggplot() +
   geom_spatraster(data = rast(m.dat))
   
 
+#### Li TIFF... other method
+
+# TIFF
+infile <- "src/maps/GAM_Favorability.tiff"
+# From TIFF summary:
+ncols <- 277
+nrows <- 197
+r <-  raster(ncols=ncols, nrows=nrows)
+r[] <- 1
+tmp <- paste(tempdir(), infile, sep = "/")
+writeRaster(r, tmp)
+
+## read now
+r <- raster(tmp)
+
+
+
+m.dat <- matrix(c(data[1,]), 
+                ncol=ncols, nrow=nrows, byrow=TRUE)
+
+m.dat[m.dat == -9999] = NA
+
+rr <- raster(m.dat,
+             crs = "+proj=lcc +lon_0=135 +lat_0=-30 +lat_1=-10 +lat_2=-45 +datum=WGS84")
 
 
 
